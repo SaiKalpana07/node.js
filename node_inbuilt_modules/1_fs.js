241c1a144c04f1608b7e1204317d775e2c76a45c
SaiKalpana07 <saireddy7991@gmail.com>
2024-04-21 23:42:10 +0530

    file creation

--- node_inbuilt_modules/1_fs.js
+++ node_inbuilt_modules/1_fs.js
@@ -5,19 +5,35 @@ const fs = require('fs')
 
 //To read a file
 
-const data = fs.readFileSync('f1.txt')
-console.log("file1"+ data)
+// const data = fs.readFileSync('f1.txt')
+// console.log("file1"+ data)
 
 // To write a file
 
-fs.writeFileSync('f2.txt',"Writing sample text into the file2")
+// fs.writeFileSync('f2.txt',"Writing sample text into the file2")
 
-fs.writeFileSync('f3.txt',"Writing data into the file that doesn't exist")
+// fs.writeFileSync('f3.txt',"Writing data into the file that doesn't exist")
 
 //  To update a file
 
-fs.appendFileSync('f2.txt', "This is my update text content")
+// fs.appendFileSync('f2.txt', "This is my update text content")
 
 // To delete a file
 
-fs.unlinkSync('f3.txt')
+// fs.unlinkSync('f3.txt')
+
+
+// To create a directory or file
+
+// fs.mkdirSync('myDirectory1')
+// fs.mkdirSync('myDirectory2')
+// fs.mkdirSync('myDirectory3')
+
+// To delete a file or directory
+
+// fs.rmdirSync('myDirectory2')
+
+//To find whether the directory exists or not
+
+const result = fs.existsSync('myDirectory1')
+console.log(result)
---
+++ node_inbuilt_modules/2_path.js
@@ -0,0 +1,5 @@
+const path = require('path')
+
+const extension = path.extname('C:\Users\\91741\Desktop\SAI REDDY\Sword Global\New folder\Node.js\node_inbuilt_modules\f1.txt')
+
+console.log(extension)
